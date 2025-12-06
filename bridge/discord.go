8aab0aca288aedcc0322c5b633a0679b7bd52989
rtk0c <mail.tianyig@gmail.com>
2025-12-06 05:14:07 +0000

    Merge discord_events.go into discord.go, and add back event handlers
    First step to add member list caching mechanism, so we don't have to look through the entire thing every time somebody sends a mention

--- bridge/discord.go
+++ bridge/discord.go
@@ -40,10 +40,18 @@ func newDiscord(bridge *Bridge, botToken, guildID string) (*discordBot, error) {
 	}
 
 	// These events are all fired in separate goroutines
-	discord.Session.AddHandler(discord.OnReady)
+	discord.Session.AddHandler(discord.onReady)
 	discord.Session.AddHandler(discord.onMessageCreate)
 	discord.Session.AddHandler(discord.onMessageUpdate)
 	discord.Session.AddHandler(discord.onGuildEmojiUpdate)
+
+	discord.Session.AddHandler(discord.onMemberListChunk)
+	discord.Session.AddHandler(discord.onMemberUpdate)
+	discord.Session.AddHandler(discord.onMemberLeave)
+	discord.Session.AddHandler(discord.onPresencesReplace)
+	discord.Session.AddHandler(discord.onPresenceUpdate)
+	discord.Session.AddHandler(discord.onTypingStart)
+	discord.Session.AddHandler(discord.onMessageReactionAdd)
 
 	return discord, nil
 }
@@ -415,4 +423,114 @@ func GetMemberNick(m *discordgo.Member) string {
 	}
 
 	return m.Nick
+}
+
+func (d *discordBot) onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	d.publishMessage(s, m.Message, false)
+}
+
+func (d *discordBot) onMessageUpdate(s *discordgo.Session, m *discordgo.MessageUpdate) {
+	d.publishMessage(s, m.Message, true)
+}
+
+func (d *discordBot) onMessageReactionAdd(s *discordgo.Session, m *discordgo.MessageReactionAdd) {
+	d.publishReaction(s, m.MessageReaction)
+}
+
+// onMemberListChunk is fired in response to our GuildMembers request in onReady
+func (d *discordBot) onMemberListChunk(s *discordgo.Session, m *discordgo.GuildMembersChunk) {
+	for _, m := range m.Members {
+		d.handleMemberUpdate(m, false)
+	}
+}
+
+func (d *discordBot) onMemberUpdate(s *discordgo.Session, m *discordgo.GuildMemberUpdate) {
+	d.handleMemberUpdate(m.Member, false)
+}
+
+// onMemberLeave is triggered when a user is removed from a guild (leave/kick/ban).
+func (d *discordBot) onMemberLeave(s *discordgo.Session, m *discordgo.GuildMemberRemove) {
+	d.bridge.removeUserChan <- m.User.ID
+}
+
+// What does this do? Probably what it sounds like.
+func (d *discordBot) onPresencesReplace(s *discordgo.Session, m *discordgo.PresencesReplace) {
+	for _, p := range *m {
+		d.handlePresenceUpdate(p.User.ID, p.Status, false)
+	}
+}
+
+// Handle when presence is updated
+func (d *discordBot) onPresenceUpdate(s *discordgo.Session, m *discordgo.PresenceUpdate) {
+	d.handlePresenceUpdate(m.Presence.User.ID, m.Presence.Status, false)
+}
+
+func (d *discordBot) onTypingStart(s *discordgo.Session, m *discordgo.TypingStart) {
+	status := discordgo.StatusOffline
+
+	p, err := d.Session.State.Presence(d.guildID, m.UserID)
+	if err != nil {
+		log.Println(errors.Wrap(err, "get presence from in onTypingStart failed"))
+		// return
+	} else {
+		status = p.Status
+	}
+
+	// .. and handle as per usual
+	d.handlePresenceUpdate(m.UserID, status, true)
+}
+
+func (d *discordBot) onReady(s *discordgo.Session, m *discordgo.Ready) {
+	// Fires a GuildMembersChunk event
+	err := d.Session.RequestGuildMembers(d.guildID, "", 0, "", true)
+	if err != nil {
+		log.Warningln(errors.Wrap(err, "could not request guild members").Error())
+		return
+	}
+
+	emoji, err := d.Session.GuildEmojis(d.guildID)
+	if err == nil {
+		d.setGuildEmoji(d.guildID, emoji)
+	}
+}
+
+func (d *discordBot) onGuildEmojiUpdate(s *discordgo.Session, m *discordgo.GuildEmojisUpdate) {
+	d.setGuildEmoji(m.GuildID, m.Emojis)
+}
+
+func (d *discordBot) setGuildEmoji(guild string, emoji []*discordgo.Emoji) {
+	d.bridge.emoji = make(map[string]*discordgo.Emoji)
+	for _, e := range emoji {
+		d.bridge.emoji[strings.ToLower(e.Name)] = e
+	}
+}
+
+func (d *discordBot) handleMemberUpdate(m *discordgo.Member, forceOnline bool) {
+	status := discordgo.StatusOnline
+
+	if !forceOnline {
+		presence, err := d.Session.State.Presence(d.guildID, m.User.ID)
+		if err != nil {
+			// This error is usually triggered on first run because it represents offline
+			if err != discordgo.ErrStateNotFound {
+				log.WithField("error", err).Errorln("presence retrieval failed")
+			}
+			return
+		}
+
+		if !isStatusOnline(presence.Status) {
+			return
+		}
+
+		status = presence.Status
+	}
+
+	d.sendUpdateUserChan(DiscordUser{
+		ID:            m.User.ID,
+		Username:      m.User.Username,
+		Discriminator: m.User.Discriminator,
+		Nick:          GetMemberNick(m),
+		Bot:           m.User.Bot,
+		Online:        isStatusOnline(status),
+	})
 }
